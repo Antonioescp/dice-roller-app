2ebabb0548e3de178cbc6fce1fbecacf675620ad
Juan Perez <packageinstaller2@gmail.com>
2022-06-03 23:29:44 -0600

    App style updated
    Drawables for a dice in all different positions have been added, code in main activity has been updated to update the image view just added

--- app/src/main/java/com/shrimpcode/diceroller/MainActivity.kt
+++ app/src/main/java/com/shrimpcode/diceroller/MainActivity.kt
@@ -20,8 +20,19 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun rollDice(sides: Int) {
-        val rolled = Random.nextInt(sides) + 1
-        binding.resultText.text = rolled.toString()
+        val diceResult = Random.nextInt(sides) + 1
+        val diceResource = when (diceResult) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            6 -> R.drawable.dice_6
+            else -> R.drawable.empty_dice
+        }
+
+        binding.diceImage.setImageResource(diceResource)
+
         makeShortToast("Dice rolled!")
     }
 }
